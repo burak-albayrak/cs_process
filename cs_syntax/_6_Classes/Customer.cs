f7a55fe9640fa38171496a29f7814377fd2ddc9e
Burak Albayrak <burak_albayrak0@icloud.com>
2023-07-17 22:05:27 +0300

    Customer.cs

---
+++ cs_syntax/_6_Classes/Customer.cs
@@ -0,0 +1,10 @@
+namespace Classes;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public String FirstName { get; set; }
+    public String LastName { get; set; }
+    public String City { get; set; }
+
+}
